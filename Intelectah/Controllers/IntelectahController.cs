a02ca3f52631813e716d330757e941e7c87c8f0b
Matheus H <matheushenriquedasilvaa.2021@gmail.com>
2022-05-19 00:07:41 -0300

    criando um paciente

--- Intelectah/Controllers/IntelectahController.cs
+++ Intelectah/Controllers/IntelectahController.cs
@@ -1,7 +1,9 @@
 ﻿using Intelectah.Data;
 using Intelectah.Models;
+using Intelectah.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,10 +13,10 @@ namespace Intelectah.Controllers
     [Route(template:"v1")]
     public class IntelectahController : ControllerBase
     {
-        //actions
+        //buscar pacientes
         [HttpGet]
         [Route(template: "pacientes")]
-        public async Task<IActionResult> Get(
+        public async Task<IActionResult> GetAsync(
             [FromServices] AppDbContext context)
         {
             var pacientes = await context
@@ -23,6 +25,56 @@ namespace Intelectah.Controllers
                 .ToListAsync();
 
             return Ok(pacientes);
+        }
+
+        //buscar paciente pelo id
+        [HttpGet]
+        [Route(template: "pacientes/{id}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromServices] AppDbContext context, [FromRoute]int id)
+        {
+            var paciente = await context
+                .Pacientes
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            //verificar se existe o item
+            return paciente == null 
+                ? NotFound() 
+                : Ok(paciente);
+        }
+
+        //Criar paciente
+        [HttpPost]
+        [Route(template: "paciente")]
+        public async Task<IActionResult> PostAsync(
+            [FromServices] AppDbContext context,
+            [FromBody]CriarPacienteViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var paciente = new Paciente
+            {
+                Nome = model.Nome,
+                Cpf = model.Cpf,
+                DataDeNascimento = model.DataDeNascimento,
+                Sexo = model.Sexo,
+                Telefone = model.Telefone,
+                Email = model.Email
+
+            };
+
+            try
+            {
+                await context.Pacientes.AddAsync(paciente);
+                await context.SaveChangesAsync();
+                return Created(uri: $"v1/paciente/{paciente.Id}", paciente);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ Intelectah/ViewModels/CriarPacienteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intelectah.ViewModels
+{
+    public class CriarPacienteViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cpf { get; set; }
+        [Required]
+        public DateTime DataDeNascimento { get; set; }
+        [Required]
+        public string Sexo { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
